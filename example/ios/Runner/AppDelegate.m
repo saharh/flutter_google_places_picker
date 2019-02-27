b23509f91d646229d669f1d61fd6ad78c3e1b07d
Sahar Haddad <saharh09@gmail.com>
2019-02-27 23:36:51 +0200

    pending result ios

--- example/ios/Runner/AppDelegate.m
+++ example/ios/Runner/AppDelegate.m
@@ -7,6 +7,10 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
   [GeneratedPluginRegistrant registerWithRegistry:self];
+    NSString *googlePlacesApiKey = @"YOUR_IOS_RESTRICTED_API_KEY";
+    [GMSPlacesClient provideAPIKey:googlePlacesApiKey];
+    [GMSServices provideAPIKey:googlePlacesApiKey];
+
   // Override point for customization after application launch.
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
